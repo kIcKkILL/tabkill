fde8cd70f5e09370a29184c86d9a10e9fadc0a2a
kIcKkILL <kikyouer@gmail.com>
2015-02-05 11:15:17 +0800

    default url rule removed

--- background.js
+++ background.js
@@ -1,31 +1,30 @@
 var queryinfo = {
-	pinned : false,
-	currentWindow : true,
-	title : "placeholder",
+    pinned : false,
+    currentWindow : true,
+    title : "placeholder",
 };
 
 function killTabs(placeholder) {
-	chrome.storage.sync.get(["rawtitles","rawurls"],function(rawrules) {
-		var titles = rawrules["rawtitles"].split('\n');
-		var urls = rawrules["rawurls"].split('\n');
-		for (i in titles) {
-			queryinfo.title = titles[i];
-			chrome.tabs.query(queryinfo,doKill);
-		}
-		delete queryinfo.title;
-		for (i in urls) {
-			queryinfo.url = urls[i];
-			chrome.tabs.query(queryinfo,doKill);
-		}
-	});
+    chrome.storage.sync.get(["rawtitles","rawurls"],function(rawrules) {
+        var titles = rawrules["rawtitles"].split('\n');
+        var urls = rawrules["rawurls"].split('\n');
+        for (i in titles) {
+            queryinfo.title = titles[i];
+            chrome.tabs.query(queryinfo,doKill);
+        }
+        delete queryinfo.title;
+        queryinfo.url = urls;
+        chrome.tabs.query(queryinfo,doKill);
+    });
 }
 
 function doKill(tabs) {
-	var tabid_array = new Array();
-	for (i in tabs) {
-		tabid_array.push(tabs[i].id);
-	}
-	chrome.tabs.remove(tabid_array);
+    var tabid_array = new Array();
+    for (i in tabs) {
+        console.log(i);
+        tabid_array.push(tabs[i].id);
+    }
+    chrome.tabs.remove(tabid_array);
 }
 
 
--- options.js
+++ options.js
@@ -1,37 +1,38 @@
 // default rules here
 var default_title = "* - Google Search";
-var default_url = "*://www.google.*/search*\n*://*.bing.com/search?q=*";
+var default_url = "";
+// var default_url = "*://www.google.*/search*\n*://*.bing.com/search?q=*";
 
 function saveRule(e) {
-	var rawtitles = document.getElementById("titles").value;
-	var rawurls = document.getElementById("urls").value;
-	chrome.storage.sync.set({'rawtitles' : rawtitles , 'rawurls' : rawurls}, function() {
-		// TODO notify user
-	});
+    var rawtitles = document.getElementById("titles").value;
+    var rawurls = document.getElementById("urls").value;
+    chrome.storage.sync.set({'rawtitles' : rawtitles , 'rawurls' : rawurls}, function() {
+        // TODO notify user
+    });
 }
 
 function defaultRule(e) {
-	// apply default rules
-	document.getElementById("titles").value = default_title;
-	document.getElementById("urls").value = default_url;
-	saveRule(e);
+    // apply default rules
+    document.getElementById("titles").value = default_title;
+    document.getElementById("urls").value = default_url;
+    saveRule(e);
 }
 
 // When options page has finished loading
 document.addEventListener('DOMContentLoaded', function () {
-	// Read from storage
-	chrome.storage.sync.get(["rawtitles","rawurls"],function(rawrules) {
-		// When first run, apply default rule
-		if (chrome.runtime.lastError) {
-			defaultRule(null);
-			return;
-		}
-		var rawtitles = rawrules["rawtitles"];
-		var rawurls = rawrules["rawurls"];
-		document.getElementById("titles").value = rawtitles;
-		document.getElementById("urls").value = rawurls;
-	});
-	// Register button click handler
-	document.querySelector('#savebut').addEventListener('click', saveRule);
-	document.querySelector('#defaultbut').addEventListener('click', defaultRule);
+    // Read from storage
+    chrome.storage.sync.get(["rawtitles","rawurls"],function(rawrules) {
+        // When first run, apply default rule
+        if (chrome.runtime.lastError) {
+            defaultRule(null);
+            return;
+        }
+        var rawtitles = rawrules["rawtitles"];
+        var rawurls = rawrules["rawurls"];
+        document.getElementById("titles").value = rawtitles;
+        document.getElementById("urls").value = rawurls;
+    });
+    // Register button click handler
+    document.querySelector('#savebut').addEventListener('click', saveRule);
+    document.querySelector('#defaultbut').addEventListener('click', defaultRule);
 });
